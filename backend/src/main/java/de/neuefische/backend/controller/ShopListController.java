841716e2b6a1f740053ca4221e93823152ddc068
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-01-21 14:49:50 +0100

    Fix/Improve ShopListController

--- backend/src/main/java/de/neuefische/backend/controller/ShopListController.java
+++ backend/src/main/java/de/neuefische/backend/controller/ShopListController.java
@@ -1,18 +1,14 @@
 package de.neuefische.backend.controller;
 
-
 import de.neuefische.backend.api.DtoListname;
-import de.neuefische.backend.model.ListItem;
 import de.neuefische.backend.model.ShopList;
 import de.neuefische.backend.service.ShopListService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
+import java.security.InvalidParameterException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/lists")
@@ -25,21 +21,19 @@ public class ShopListController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<ShopList>> getAllLists(){
-        return ok(shopListService.getAll());
+    public List<ShopList> getAllLists(){
+        return shopListService.getAll();
     }
 
     @PostMapping()
-    public ResponseEntity<ShopList> postNewList(@RequestBody DtoListname listName){
+    public ShopList postNewList(@RequestBody DtoListname listName){
         Optional<ShopList> optionalShopList = shopListService.saveNewList(listName.getListName());
-        if (optionalShopList.isEmpty()){
-            return ResponseEntity.badRequest().build();
-        }
-        return ok(optionalShopList.get());
+        return optionalShopList.orElseThrow(() -> new InvalidParameterException("List with Name: " + listName.getListName() + " already exists!"));
     }
 
     @GetMapping("/{whichList}")
-    public ResponseEntity<ShopList> getListByName (@RequestParam String whichList){
+    public ShopList getListByName (@PathVariable String whichList){
         Optional<ShopList> optionalShopList = shopListService.findByName(whichList);
+        return optionalShopList.orElseThrow(() -> new NoSuchElementException("List with Name: " + whichList + " not found!"));
     }
 }
